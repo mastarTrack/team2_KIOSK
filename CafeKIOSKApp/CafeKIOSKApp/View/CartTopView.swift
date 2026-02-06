d9c7ebb7828938a494ffd01dbc54673e32bb9ecc
coduhee <vdyjb@ewha.ac.kr>
2026-02-06 10:21:57 +0900

    feat: CartTopView - 주문하기 페이지 상단뷰 구현

---
+++ CafeKIOSKApp/CafeKIOSKApp/View/CartTopView.swift
@@ -0,0 +1,71 @@
+//
+//  CartTopView.swift
+//  CafeKIOSKApp
+//
+//  Created by 김주희 on 2/6/26.
+//
+
+import UIKit
+import SnapKit
+import Then
+import Kingfisher
+
+class CartTopView: UIView {
+
+    private let cartTopLabel = UILabel().then {
+        $0.backgroundColor = .systemBackground
+        $0.textColor = .black
+        $0.font = .systemFont(ofSize: 22, weight: .medium)
+        $0.textAlignment = .center
+        $0.text = "주문하기"
+    }
+    
+    private let locationView = UIView().then {
+        $0.backgroundColor = .systemGray5
+    }
+    
+    private let locationLabel = UILabel().then {
+        $0.textColor = .gray
+        $0.font = .systemFont(ofSize: 18, weight: .semibold)
+        $0.text = "       칼퇴커피 1호점"
+    }
+    
+    // 초기화
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // UI설정
+    private func setupUI() {
+        self.backgroundColor = .white
+        
+        self.addSubview(cartTopLabel)
+        self.addSubview(locationView)
+        locationView.addSubview(locationLabel)
+        
+        cartTopLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().offset(4)
+        }
+        
+        locationView.snp.makeConstraints {
+            $0.top.equalTo(cartTopLabel.snp.bottom).offset(10)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(45)
+        }
+        
+        locationLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(10)
+            $0.centerY.equalToSuperview()
+        }
+    }
+}
+
+#Preview {
+    CartTableViewController()
+}
