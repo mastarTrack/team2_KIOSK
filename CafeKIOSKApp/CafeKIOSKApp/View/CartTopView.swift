fbc3ecbd9082c26111305018e82c69cd9b1ead3a
coduhee <vdyjb@ewha.ac.kr>
2026-02-09 00:31:20 +0900

    refactor: UI요소 수정

--- CafeKIOSKApp/CafeKIOSKApp/View/CartTopView.swift
+++ CafeKIOSKApp/CafeKIOSKApp/View/CartTopView.swift
@@ -11,14 +11,8 @@ import Then
 
 class CartTopView: UIView {
     
-    // 주문하기 라벨
-    private let cartTopLabel = UILabel().then {
-        $0.backgroundColor = .systemBackground
-        $0.textColor = .black
-        $0.font = .systemFont(ofSize: 22, weight: .medium)
-        $0.textAlignment = .center
-        $0.text = "주문하기"
-    }
+    // 뷰컨한테 전체선택 버튼 눌렸다고 전하는 심부름꾼 클로저
+    var selectAllAction: (() -> Void)?
     
     // 위치 나타내는 부분 뷰
     private let locationView = UIView().then {
@@ -36,22 +30,21 @@ class CartTopView: UIView {
     // 위치 라벨
     private let locationLabel = UILabel().then {
         $0.textColor = .gray
-        $0.font = .systemFont(ofSize: 18, weight: .semibold)
+        $0.font = .systemFont(ofSize: 17, weight: .semibold)
         $0.text = "칼퇴커피 1호점"
     }
     
     // 전체선택 체크 버튼 구현
     private let totalCheckButton = UIButton().then {
-        let config = UIImage.SymbolConfiguration(pointSize: 22, weight: .bold)
+        let config = UIImage.SymbolConfiguration(pointSize: 18, weight: .bold)
         let image = UIImage(systemName: "checkmark.circle.fill",  withConfiguration: config)
         $0.setImage(image, for: .normal)
         $0.tintColor = .systemGray4
     }
     
-    
     // 전체선택 텍스트 라벨
     private let totalLabel = UILabel().then {
-        $0.font = .systemFont(ofSize: 16, weight: .bold)
+        $0.font = .systemFont(ofSize: 16, weight: .medium)
     }
     
     // 초기화
@@ -64,25 +57,22 @@ class CartTopView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // 뷰컨한테 버튼 눌렸다고 전하는 심부름꾼 클로저
-    var selectAllAction: (() -> Void)?
-
     @objc private func didTapSelectAll() {
         selectAllAction?()
     }
     
+    // 전체선택 섹션 업데이트 메서드
     func updateStatus(selectedCount: Int, totalCount: Int) {
-        let text = "전체선택 \(selectedCount)/\(totalCount)"
         
+        // 전체선택 텍스트 레이블
+        let text = "전체선택 \(selectedCount)/\(totalCount)"
         totalLabel.text = text
         
         let isAllSelected = (totalCount > 0) && (selectedCount == totalCount)
-        
-        
         totalCheckButton.isSelected = isAllSelected
         
+        // 전체선택되어있으면 노란색으로 색상 변경
         if isAllSelected {
-            totalCheckButton.setImage(UIImage(systemName: "checkmark.circle.fill"), for: .normal)
             totalCheckButton.tintColor = .systemYellow
         } else {
             totalCheckButton.tintColor = .systemGray4
@@ -90,30 +80,25 @@ class CartTopView: UIView {
     }
     
     
-    // UI설정
+    // MARK: -- UI설정
     private func setupUI() {
         self.backgroundColor = .white
         
-        self.addSubview(cartTopLabel)
         self.addSubview(locationView)
         self.addSubview(totalCheckButton)
         self.addSubview(totalLabel)
+        
         locationView.addSubview(mapButton)
         locationView.addSubview(locationLabel)
         
-        cartTopLabel.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.top.equalToSuperview().offset(4)
-        }
-        
         locationView.snp.makeConstraints {
-            $0.top.equalTo(cartTopLabel.snp.bottom).offset(10)
             $0.leading.trailing.equalToSuperview()
-            $0.bottom.equalToSuperview().inset(45)
+            $0.top.equalToSuperview()
+            $0.height.equalTo(40)
         }
         
         locationLabel.snp.makeConstraints {
-            $0.leading.equalTo(mapButton.snp.trailing).offset(10)
+            $0.leading.equalTo(mapButton.snp.trailing).offset(3)
             $0.centerY.equalToSuperview()
         }
         
@@ -124,16 +109,20 @@ class CartTopView: UIView {
         
         totalCheckButton.snp.makeConstraints {
             $0.top.equalTo(locationView.snp.bottom).offset(8)
-            $0.leading.equalToSuperview().inset(26)
+            $0.leading.equalToSuperview().inset(27)
         }
         
         totalLabel.snp.makeConstraints {
-            $0.leading.equalTo(totalCheckButton.snp.trailing).offset(4)
+            $0.top.equalTo(locationView.snp.bottom).offset(8)
+            $0.leading.equalTo(totalCheckButton.snp.trailing).offset(5)
             $0.centerY.equalTo(totalCheckButton)
         }
     }
 }
 
+
+
 #Preview {
-    CartViewController()
+    UINavigationController(rootViewController: CartViewController())
 }
+
